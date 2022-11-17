375c0d4bc0ca699b0bb0730b32203003039e6cdf
Juan Felipe Gonzalez <felipegonzalezmv@hotmail.com>
2022-11-17 17:03:52 -0500

    Add styles to register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
-import { async } from "q";
+import { useNavigate, Link } from "react-router-dom";
+import { Alert } from "../components";
 
 export const Register = () => {
   const { signUp } = useAuth();
@@ -12,11 +12,6 @@ export const Register = () => {
   });
 
   const [error, setError] = useState("");
-
-  const handleChange = ({ target: { name, value } }) => {
-    setError("");
-    setUser({ ...user, [name]: value });
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,27 +25,53 @@ export const Register = () => {
   };
 
   return (
-    <div>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="youremail@example.com"
-          onChange={handleChange}
-        />
+    <div className="w-full max-w-xs m-auto text-black">
+      {error && <Alert message={error} />}
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="*****"
-          onChange={handleChange}
-        />
-        <button>Register</button>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-6 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="youremail@example.com"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="*************"
+          />
+        </div>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Register
+        </button>
       </form>
+      <p className="my-4 text-sm flex justify-between px-3">
+        Already have an Account?
+        <Link to="/login" className="text-blue-700 hover:text-blue-900">
+          Login
+        </Link>
+      </p>
     </div>
   );
 };
