bb56ef0f1d1308c1205a7f4b301617aa68395a27
Juan Felipe Gonzalez <felipegonzalezmv@hotmail.com>
2022-11-17 16:09:17 -0500

    Add Context api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { Home, Login, Register } from "./components";
+import { AuthProvider } from "./context/authContext";
+
 export default function App() {
   return (
     <div className="bg-slate-300 text-black h-screen flex text-white">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<Register />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Register />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useAuth } from "../context/authContext";
 
 export const Home = () => {
+  const { user } = useAuth();
+
+  console.log("user :>> ", user);
+
   return <div>Home</div>;
 };
